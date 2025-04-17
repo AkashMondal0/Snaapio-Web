1ec61d6c8d5956b3371b4414a0703b5db3bbd7ea
Akash Mondal <akash2003mondal@gmail.com>
2025-04-17 21:53:39 +0530

    feat: Update package dependencies and improve component structure with dynamic imports

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,9 +1,17 @@
 "use client"
-
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
+import Redux_Provider from "./Redux_Provider"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <html lang="en" suppressHydrationWarning={false}>
+    <body className="ease-in-out duration-300 ">
+      <Redux_Provider>
+        <NextThemesProvider {...props}>
+          {children}
+        </NextThemesProvider>
+      </Redux_Provider>
+    </body>
+  </html>
 }
